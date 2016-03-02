caedbb4d4a88bc7fd70929efa1b3991839b73941
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-03-02 14:35:27 -0500

    Added django-configurations with mixins. Disabled whitenoise for now.

--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from .settings_deployment import *
-from .settings_jurisdiction import *
-
+from .settings_deployment import DeploymentConfig
+from .settings_jurisdiction import JurisdictionConfig
+from configurations import Configuration, values
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,90 +22,90 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
+class Default(DeploymentConfig, JurisdictionConfig, Configuration):
+
+    ALLOWED_HOSTS = [
+        'localhost', '127.0.0.1',
+        '.datamade.us',
+        '.councilmatic.org'
+    ]
 
 
-ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1',
-    '.datamade.us',
-    '.councilmatic.org'
-]
+    # Application definition
 
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'haystack',
+        'councilmatic_core',
+        'chicago',
+    )
 
-# Application definition
+    try:
+        INSTALLED_APPS += EXTRA_APPS
+    except NameError:
+        pass
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'haystack',
-    'councilmatic_core',
-    'chicago',
-)
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.cache.UpdateCacheMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'django.middleware.security.SecurityMiddleware',
+        'django.middleware.cache.FetchFromCacheMiddleware',
+    )
 
-try:
-    INSTALLED_APPS += EXTRA_APPS
-except NameError:
-    pass
+    ROOT_URLCONF = 'councilmatic.urls'
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
-)
-
-ROOT_URLCONF = 'councilmatic.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'councilmatic_core.views.city_context',
-            ],
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'councilmatic_core.views.city_context',
+                ],
+            },
         },
-    },
-]
+    ]
 
-WSGI_APPLICATION = 'councilmatic.wsgi.application'
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/Chicago'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+    WSGI_APPLICATION = 'councilmatic.wsgi.application'
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'America/Chicago'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
 
 
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+    HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+    #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- councilmatic/wsgi.py
+++ councilmatic/wsgi.py
@@ -13,7 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "councilmatic.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Default")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
