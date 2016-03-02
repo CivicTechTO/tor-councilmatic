6ab6974adb166018b3f6b31134aa5b6f20a985a2
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-03-02 04:31:18 -0500

    Use overridable settings variables via django-configurations.

--- councilmatic/wsgi.py
+++ councilmatic/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "councilmatic.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Default")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
