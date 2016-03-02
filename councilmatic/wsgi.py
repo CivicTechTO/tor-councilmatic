19a55ad0e867a8b3f7943a2200a26a3b27364d63
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-03-02 14:17:41 -0500

    Remove django-configurations to get working.

--- councilmatic/wsgi.py
+++ councilmatic/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "councilmatic.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Default")
 
