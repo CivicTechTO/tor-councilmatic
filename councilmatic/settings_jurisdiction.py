33ece0d5e1615190e3ae56b34777d4e6529bce2c
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-10-19 22:49:19 -0400

    Added old metropolitan toronto logo.

--- councilmatic/settings_jurisdiction.py
+++ councilmatic/settings_jurisdiction.py
@@ -112,7 +112,7 @@ class JurisdictionConfig(object):
 
     # these should live in APP_NAME/static/
     IMAGES = {
-        'logo': 'images/logo.png',
+        'logo': 'images/metro-toronto-logo.png',
     }
     # you can generate icons from the logo at http://www.favicomatic.com/
     # & put them in APP_NAME/static/images/icons/
